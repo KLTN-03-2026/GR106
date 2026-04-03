82eb121595e5fcf767e22fe4244b52a1cc4026d7
Dũng <lxd0294@gmail.com>
2026-04-03 19:18:05 +0700

    add Validation LoginRequest and VerifyRequest

--- src/main/java/com/farmapp/farmsmartmanagement/dto/request/auth/LoginRequest.java
+++ src/main/java/com/farmapp/farmsmartmanagement/dto/request/auth/LoginRequest.java
@@ -1,6 +1,12 @@
 package com.farmapp.farmsmartmanagement.dto.request.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record LoginRequest(
+        @NotBlank(message = "Email không được để trống")
         String email,
+
+        @NotBlank(message = "Mật khẩu không được để trống")
         String password
 ) {}
--- src/main/java/com/farmapp/farmsmartmanagement/dto/request/auth/VerifyRequest.java
+++ src/main/java/com/farmapp/farmsmartmanagement/dto/request/auth/VerifyRequest.java
@@ -3,5 +3,5 @@ package com.farmapp.farmsmartmanagement.dto.request.auth;
 import jakarta.validation.constraints.NotBlank;
 
 public record VerifyRequest(
-        @NotBlank String token
+        @NotBlank(message = "Xác thực tài khoản thất bại, vui lòng kiểm tra lại token") String token
 ) {}
