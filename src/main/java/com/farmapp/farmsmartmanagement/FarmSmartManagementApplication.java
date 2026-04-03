123af290e4866a172229f9e909e1fc94451a703e
Dũng <lxd0294@gmail.com>
2026-04-03 13:37:55 +0700

    fix: exclude RedisRepositoriesAutoConfiguration

--- src/main/java/com/farmapp/farmsmartmanagement/FarmSmartManagementApplication.java
+++ src/main/java/com/farmapp/farmsmartmanagement/FarmSmartManagementApplication.java
@@ -2,12 +2,13 @@ package com.farmapp.farmsmartmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        RedisRepositoriesAutoConfiguration.class
+})
 public class FarmSmartManagementApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FarmSmartManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FarmSmartManagementApplication.class, args);
+    }
 }
