5ea129b0ecf8cd74c52be6c249707c2a01deada8
Dũng <lxd0294@gmail.com>
2026-04-16 21:00:07 +0700

    PlanStage: add CreateRequest and Mapper

--- src/main/java/com/farmapp/farmsmartmanagement/modules/plan/mapper/PlanStageMapper.java
+++ src/main/java/com/farmapp/farmsmartmanagement/modules/plan/mapper/PlanStageMapper.java
@@ -1,4 +1,17 @@
 package com.farmapp.farmsmartmanagement.modules.plan.mapper;
 
-public class PlanStageMapper {
+import com.farmapp.farmsmartmanagement.infrastructure.persistence.entity.PlanStageEntity;
+import com.farmapp.farmsmartmanagement.modules.plan.dto.response.PlanStageResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PlanStageMapper {
+
+    @Mapping(source = "plan.id", target = "planId")
+    PlanStageResponse toResponse(PlanStageEntity planStageEntity);
+
+    List<PlanStageResponse> toResponses(List<PlanStageEntity> planStageEntity);
 }
---
+++ src/main/java/com/farmapp/farmsmartmanagement/modules/plan/mapper/TaskMapper.java
@@ -0,0 +1,4 @@
+package com.farmapp.farmsmartmanagement.modules.plan.mapper;
+
+public interface TaskMapper {
+}
