3965a6aedcd92c80e6d07faf8d7ca4549680b716
Dũng <lxd0294@gmail.com>
2026-04-19 00:04:27 +0700

    Create PlanPlotRepository

---
+++ src/main/java/com/farmapp/farmsmartmanagement/infrastructure/persistence/repository/PlanPlotRepository.java
@@ -0,0 +1,24 @@
+package com.farmapp.farmsmartmanagement.infrastructure.persistence.repository;
+
+import com.farmapp.farmsmartmanagement.infrastructure.persistence.entity.PlanPlotEntity;
+import com.farmapp.farmsmartmanagement.infrastructure.persistence.entity.PlotEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PlanPlotRepository extends JpaRepository<PlanPlotEntity, UUID> {
+    boolean existsByPlanIdAndPlotId(UUID planId, UUID plotId);
+
+    @Query("""
+    SELECT pp.plot FROM PlanPlotEntity pp
+    WHERE pp.plan.id = :planId
+    AND pp.plot.id = :plotId
+""")
+    Optional<PlotEntity> findPlotByPlanIdAndPlotId(
+            @Param("planId") UUID planId,
+            @Param("plotId") UUID plotId
+    );
+}
